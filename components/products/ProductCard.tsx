155a135c142c79753c26f4164f20381573d4882d
ljaviertovar <luisjavier_tovar@outlook.com>
2023-01-24 21:22:57 -0500

    add ProductCard

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,19 +1,31 @@
-import { Card, CardActionArea, CardMedia, Grid } from "@mui/material"
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material"
 
 import { Product } from "../../interfaces/products.interface"
+import { useMemo, useState } from "react"
 
 interface Props {
 	product: Product
 }
 
 export default function ProductCard({ product }: Props) {
+	const [isHovered, setIsHovered] = useState(false)
+
+	const productImage = useMemo(() => {
+		return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}`
+	}, [isHovered, product.images])
+
 	return (
-		<Grid item xs={6} sm={4}>
+		<Grid item xs={6} sm={4} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
 			<Card>
 				<CardActionArea>
-					<CardMedia component='img' image={`products/${product.images[0]}`} alt={product.title} />
+					<CardMedia component='img' image={productImage} alt={product.title} className='fadeIn' />
 				</CardActionArea>
 			</Card>
+
+			<Box sx={{ mt: 1 }} className='fadeIn'>
+				<Typography fontWeight={700}>{product.title}</Typography>
+				<Typography fontWeight={500}>${product.price}</Typography>
+			</Box>
 		</Grid>
 	)
 }
