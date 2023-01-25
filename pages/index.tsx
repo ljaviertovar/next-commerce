1078f2189fe7adbc8c3bbaae858c536b6761c4fd
ljaviertovar <luisjavier_tovar@outlook.com>
2023-01-24 20:12:46 -0500

    add products grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material"
+import { Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material"
 import { ShopLayout } from "../components/layouts"
+
+import { initialData } from "../database"
 
 export default function Home() {
 	return (
@@ -10,6 +12,18 @@ export default function Home() {
 			<Typography variant='h2' sx={{ mb: 1 }}>
 				All Products
 			</Typography>
+
+			<Grid container spacing={4}>
+				{initialData.products.map(product => (
+					<Grid item xs={6} sm={4} key={product.slug}>
+						<Card>
+							<CardActionArea>
+								<CardMedia component='img' image={`products/${product.images[0]}`} alt={product.title} />
+							</CardActionArea>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
 		</ShopLayout>
 	)
 }
