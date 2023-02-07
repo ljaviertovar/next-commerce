dfeee142d856542a055e9d3d27c0f208622b3acd
ljaviertovar <luisjavier_tovar@outlook.com>
2023-02-06 21:50:53 -0500

    create JWT token

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import { db } from "../../../database"
 import { User } from "../../../models"
 import bcrypt from "bcryptjs"
+import { signToken } from "../../../utils/jwt"
 
 type Data =
 	| {
@@ -38,10 +39,12 @@ const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 	if (!bcrypt.compareSync(password, user.password!))
 		return res.status(400).json({ message: "Invalid Email or Password" })
 
-	const { role, name } = user
+	const { role, name, _id } = user
+
+	const token = signToken(_id, email)
 
 	return res.status(200).json({
-		token: "",
+		token,
 		user: {
 			email,
 			role,
---
+++ utils/jwt.ts
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken"
+
+export const signToken = (_id: string, email: string) => {
+	if (!process.env.JWT_SECRET_SEED) throw new Error("No JWT Seed found")
+
+	return jwt.sign({ _id, email }, process.env.JWT_SECRET_SEED, { expiresIn: "30d" })
+}
